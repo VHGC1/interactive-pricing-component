5547727b595504fd1423a4434eb8175a91c10e30
vitor costa <vitorjjhg@hotmail.com>
2021-06-07 20:04:19 -0300

    function

--- index.js
+++ index.js
@@ -5,3 +5,24 @@
 - 500k pageviews / $24 per month
 - 1M pageviews / $36 per month
 */
+
+
+let slider = document.getElementById("myRange");
+let num = document.getElementById("demo");
+
+num.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  
+  num.innerHTML = this.value
+
+  switch(this.value) {
+    case 50000:
+      num.innerHTML = "10K"
+      break
+  }
+  
+}
+
+
